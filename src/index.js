32837bf69ca4a3dfbbe61beb554de8167a6ebc4e
Angel yang <angelyang@Angels-MacBook-Pro.local>
2017-03-26 11:25:17 -0700

    made changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-let message = 'hello from entry';
+import React from 'react';
+import { render } from 'react-dom';
+import App from 'components/app';
 
-console.log(message);
+render(<App />, document.getElementById('app'));
